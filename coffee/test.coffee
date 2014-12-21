b9b134c0271de717ee2cc09135c3cf6b9a7d704c
ueno <allo.flash.as@gmail.com>
2014-12-21 18:22:53 +0900

    coffeescript / deferred 試す
    ほか、promiseも
    D3,js download

--- coffee/test.coffee
+++ coffee/test.coffee
@@ -1,7 +1,55 @@
 
-hoge () ->
+hoge  = ->
 	console.log 'aaa'
 	console.log 'git test'
 
-hege()
+hoge()
+
+
+
+
+ajaxSucsessHandler = (data , callback) ->
+	callback(data.userId);
+
+getUserId = ( callback ) ->
+
+	defer = $.Deferred();
+
+	$.ajax({
+		url: '/sample/user.json',
+		dataType: 'json',
+		success: (json) ->
+			data = json
+			ajaxSucsessHandler( data , callback )
+			defer.promise();
+	})
+	.done ->
+		console.log("success");
+
+	.fail ->
+		console.log("error");
+
+	.always ->
+		console.log("complete");
+
+
+
+
+
+
+$ ->
+	promise = getUserId ( userId ) ->
+		alert('ユーザーidは' + userId);
+	#	alert('hallo' + userId);
+
+	promise.done ->
+		console.log('toshi!');
+		@
+	@
+
+
+
+
+
+
 
--- js/test.js
+++ js/test.js
@@ -1,9 +1,48 @@
 (function() {
-  hoge(function() {
+  var ajaxSucsessHandler, getUserId, hoge;
+
+  hoge = function() {
     console.log('aaa');
     return console.log('git test');
-  });
+  };
 
-  hege();
+  hoge();
+
+  ajaxSucsessHandler = function(data, callback) {
+    return callback(data.userId);
+  };
+
+  getUserId = function(callback) {
+    var defer;
+    defer = $.Deferred();
+    return $.ajax({
+      url: '/sample/user.json',
+      dataType: 'json',
+      success: function(json) {
+        var data;
+        data = json;
+        ajaxSucsessHandler(data, callback);
+        return defer.promise();
+      }
+    }).done(function() {
+      return console.log("success");
+    }).fail(function() {
+      return console.log("error");
+    }).always(function() {
+      return console.log("complete");
+    });
+  };
+
+  $(function() {
+    var promise;
+    promise = getUserId(function(userId) {
+      return alert('ユーザーidは' + userId);
+    });
+    promise.done(function() {
+      console.log('toshi!');
+      return this;
+    });
+    return this;
+  });
 
 }).call(this);
